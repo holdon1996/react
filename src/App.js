9ed207cce32c6e03d70f042a56f140d70694fe70
thachvd <vuducthach25@gmail.com>
2020-03-02 14:03:57 +0700

    defaultProps

--- src/App.js
+++ src/App.js
@@ -3,64 +3,19 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      data:
-        [
-          {
-            "id": 1,
-            "name": "Foo",
-            "age": "20"
-          },
-          {
-            "id": 2,
-            "name": "Bar",
-            "age": "30"
-          },
-          {
-            "id": 3,
-            "name": "Baz",
-            "age": "40"
-          }
-        ]
-    }
-  }
   render() {
     return (
       <div>
-        <Header />
-        <table>
-          <tbody>
-            {this.state.data.map((data, index) => <TableRow key={index} data={data} />)}
-          </tbody>
-        </table>
+        <h1>{this.props.headerProp}</h1>
+        <h2>{this.props.contentProp}</h2>
       </div>
     );
   }
 }
 
-class Header extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Header</h1>
-      </div>
-    );
-  }
-}
-
-class TableRow extends React.Component {
-  render() {
-    return (
-      <tr>
-        <td>{this.key}</td>
-        <td>{this.props.data.id}</td>
-        <td>{this.props.data.name}</td>
-        <td>{this.props.data.age}</td>
-      </tr>
-    );
-  }
+App.defaultProps = {
+  headerProp: "Header from props...",
+  contentProp: "Content from props..."
 }
 
 export default App;
