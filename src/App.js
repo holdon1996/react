c5c635a0e3c4e82abbef7d292d11ff82d78b8bc9
thachvd <vuducthach25@gmail.com>
2020-03-02 12:34:12 +0700

    Lession1: hello world

--- src/App.js
+++ src/App.js
@@ -2,25 +2,25 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      header: "Header",
+      footer: "Footer"
+    }
+  }
+
+  render() {
+    const elements = (
+      <div className="firstLession">
+        <h1>{this.state.header}</h1>
+        <h2>{this.state.footer}</h2>
+      </div>
+    );
+    return elements;
+  }
 }
 
 export default App;
