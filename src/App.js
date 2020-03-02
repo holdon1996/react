6fd71dfb15a5b4d2bf63b77bad93534e3e1a43a1
thachvd <vuducthach25@gmail.com>
2020-03-02 13:17:19 +0700

    using props instead of state

--- src/App.js
+++ src/App.js
@@ -3,20 +3,11 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      header: "Header",
-      footer: "Footer"
-    }
-  }
-
   render() {
     const elements = (
       <div className="firstLession">
-        <h1>{this.state.header}</h1>
-        <h2>{this.state.footer}</h2>
+        <h1>{this.props.header}</h1>
+        <h2>{this.props.footer}</h2>
       </div>
     );
     return elements;
